94bd103ca0c3558fb47382c4bce748db0e144a4a
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-10-27 02:39:49 +0530

    Fixed info box component

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
+import { type ReactNode } from "react";
 import { CourseGoalProps } from "../App";
 import CourseGoal from "./CourseGoal";
+import InfoBox from "./InfoBoxProps";
 
 export type CourseGoalListProps = {
   goals: CourseGoalProps[];
@@ -10,21 +12,37 @@ export default function CourseGoalList({
   goals,
   onDelete
 }: CourseGoalListProps) {
+  let warningBox: ReactNode;
+  if (goals.length === 0) {
+    return <InfoBox mode="hint">You are not having any goals yet</InfoBox>;
+  }
+
+  if (goals.length >= 2) {
+    warningBox = (
+      <InfoBox mode="warning" severity="high">
+        You are having too much goals , dont put so much on your plate
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map(({ id, title, description }: CourseGoalProps) => {
-        return (
-          <li key={id}>
-            <CourseGoal
-              id={id}
-              title={title}
-              description={description}
-              key={title}
-              onDelete={onDelete}
-            />
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map(({ id, title, description }: CourseGoalProps) => {
+          return (
+            <li key={id}>
+              <CourseGoal
+                id={id}
+                title={title}
+                description={description}
+                key={title}
+                onDelete={onDelete}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
