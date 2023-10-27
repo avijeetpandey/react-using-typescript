bd9b2ecc4d3e8f61c0516d791c0161deb6787bbd
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-10-27 17:08:27 +0530

    Add: form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,12 @@
-import { type FormEvent, type ComponentPropsWithoutRef } from "react";
+import { type FormEvent, type ComponentPropsWithoutRef, useRef } from "react";
 
 export type FormProps = ComponentPropsWithoutRef<"form"> & {
   onSave: (value: unknown) => void;
 };
 
 export default function Form({ children, onSave, ...otherProps }: FormProps) {
+  const formRef = useRef<HTMLFormElement>(null);
+
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
@@ -12,9 +14,12 @@ export default function Form({ children, onSave, ...otherProps }: FormProps) {
     const data = Object.fromEntries(formData);
 
     onSave(data);
+
+    // clearing the form
+    formRef.current?.reset();
   }
   return (
-    <form onSubmit={handleSubmit} {...otherProps}>
+    <form onSubmit={handleSubmit} {...otherProps} ref={formRef}>
       {children}
     </form>
   );
