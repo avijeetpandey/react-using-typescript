c7e127b96911279e4e529239e9a0a36bcd76e912
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-10-27 16:20:36 +0530

    Add: type predicate

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -15,3 +15,26 @@ export function Button(props: AnchorProps | ButtonProps) {
 
   return <button className="button" {...props}></button>;
 }
+
+// Another way of doing it
+
+export type ButtonPropsV2 = {
+  href?: never;
+} & ComponentPropsWithoutRef<"button">;
+
+export type AnchorPropsV2 = { href?: string } & ComponentPropsWithoutRef<"a">;
+
+// creating a type predicate
+function isAnchorProps(
+  props: ButtonPropsV2 | AnchorPropsV2
+): props is AnchorPropsV2 {
+  return "href" in props;
+}
+
+export function ButtonV2(props: AnchorPropsV2 | ButtonPropsV2) {
+  if (isAnchorProps(props)) {
+    return <a className="button" {...props}></a>;
+  }
+
+  return <button className="button" {...props}></button>;
+}
